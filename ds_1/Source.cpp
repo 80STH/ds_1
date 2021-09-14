ee621aa0d3dd65116b8ebe37dd78c300140d5110
80STH <84912144+80STH@users.noreply.github.com>
2021-09-14 20:51:26 +0600

    Update Source.cpp

--- ds_1/Source.cpp
+++ ds_1/Source.cpp
@@ -9,7 +9,7 @@ void main()
 	int N;
 	printf_s("Ââåäèòå x, y è ðàäèóñ êðóãà:");
 	scanf_s(" %f %f %f", &x, &y, &R);
-	if (pow(x, 2) + pow(y, 2) == pow(R, 2)) N = 1;
+	if (pow(x, 2) + pow(y, 2) <= pow(R, 2)) N = 1;
 		else N = 0;
 	if (N == 1) printf_s("Òî÷êà ëåæèò â îáëàñòè êðóãà");
 		else printf_s("Òî÷êà íå ëåæèò â îáëàñòè êðóãà");
